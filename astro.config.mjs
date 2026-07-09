dd8ca8cf7b3a755a4f2b689e8653abc135a1e4e0
nisal-wickramage <nisalwickramage@gmail.com>
2026-07-09 09:42:57 +0800

    update web site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   // Update 'site' to your GitHub Pages URL
   // For project repos: https://username.github.io/repository-name
   // For user/org repos: https://username.github.io
-  site: 'https://nisalwickramage.github.io/engineered-momentum-web-site',
+  site: 'https://nisal-wickramage.github.io/engineered-momentum-web-site/',
   
   // Uncomment if using a project repo (not username.github.io)
   // base: '/engineered-momentum-web-site',
